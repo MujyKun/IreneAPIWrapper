f7458cb10f5974e17d35e4ed96b63e50d8e7bd76
MujyKun <mujy@irenebot.com>
2022-08-05 16:52:04 -0400

    Fetch winners of a bias game

--- IreneAPIWrapper/models/biasgame.py
+++ IreneAPIWrapper/models/biasgame.py
@@ -6,6 +6,26 @@ class BiasGame:
         ...
 
     @staticmethod
+    async def fetch_winners(user_id, limit=15) -> dict:
+        """
+        Fetch the winners of a user's bias game.
+
+        :param user_id: int
+            User ID to return results for.
+        :param limit: int
+            Number of results should be retrieved in descending order.
+        :return: dict
+            Dictionary of person IDs to the amount of times they've won.
+        """
+        callback = await basic_call(request={
+            'route': 'biasgame/winners',
+            'user_id': user_id,
+            'limit': limit,
+            'method': 'POST'
+        })
+        return callback.response.get("results")
+
+    @staticmethod
     async def generate_pvp(first_file_name, second_file_name):
         """
         Generate a PvP image and return an image url.
