935a61003e1db21168bf3676c54df03bbad88292
MujyKun <mujy@irenebot.com>
2023-03-22 23:04:14 -0400

    Added stats updater

---
+++ IreneAPIWrapper/models/StatsUpdater.py
@@ -0,0 +1,22 @@
+from . import (
+    basic_call
+)
+
+
+class StatsUpdater:
+    """
+    Class that handles the update of statistics to the API.
+
+    """
+    @staticmethod
+    async def update(self, key, value):
+        """Update a stat value in the API."""
+        await basic_call(
+            request={
+                "route": "bot/updatestats",
+                "key": key,
+                "value": value,
+                "method": "PUT"
+            }
+        )
+
